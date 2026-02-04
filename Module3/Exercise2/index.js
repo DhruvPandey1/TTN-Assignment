58efe2778216da1ac1597a3d6fcfff20cf98f637
Dhruvpandey1 <dhruvpandey0597@gmail.com>
2026-02-04 23:39:21 +0530

    All required comments for Question 3 is added

--- Module3/Exercise2/index.js
+++ Module3/Exercise2/index.js
@@ -1,3 +1,5 @@
+//Q1 create a Calculator Object
+
 var calculator={
     num1:0,
     num2:0,
@@ -20,6 +22,12 @@ var calculator={
         
     }
 }
+
+/*Q2 Create a temperature object having temperature in celsius and four function 
+read() for reading temperature
+toFahrenheit() for converting temperature into fahrenheit
+toKelvin() for converting the temperature into kelvin
+display() for showing all temperature values */
 
 
 var temperatureConverter={
@@ -44,6 +52,85 @@ var temperatureConverter={
     }
 }
 
+
+/* Q3 Tasks:
+(a) Predict the output of the given snippet.
+
+(b) Explain how hoisting affects the execution 
+of console.log(z) in second().
+
+(c) Explain the scope chain for console.log(y) in 
+second().
+
+(d) What happens when console.log(y) 
+is executed outside first() (Line 4)? Why?
+
+
+(e) Modify the code to use let instead of var and 
+observe any differences.
+*/
+
+//(a)
+var x=5;
+function first(){
+    console.log(x);
+    var y=10;
+    function second(){
+        console.log(y);
+        console.log(z);
+        var z=20;
+    }
+    second();
+}
+
+first();
+console.log(y);
+
+/*console.log(x) gives 5//Line1
+​​console.log(y) gives 10//Line2
+console.log(z) gives undefined//Line3
+console.log(y) gives a reference error//Line4
+*/
+
+/*(b) In second function at execution time the declaration of var
+z=20; will get sifted to the top of the stack and the initialization 
+will remain at the same line making the value of the var z
+undefined. This whole process is called Hoisting and
+console.log(z) gives undefined
+function second(){
+​
+var z;
+​
+console.log(y);
+​
+console.log(z);
+​
+z=20;
+}
+*/
+
+/*(c) For the console.log(y) scope chaining is applied scope
+chaining is a feature of javascript which let a function access a
+variable present inside its parent or grandparent or great
+grandparent i.e It is basically first search for variable insides
+own scope if it did not find it will move to parent scope this
+process will continue till the variable has found or the scope
+ends giving a Reference Error.
+
+For console.log(y) it will search in second() function first then
+move to the first() function and find it there giving a value of 10
+*/
+
+/*(d) When the console.log(y) is executed outside first() we will
+get a Reference Error that is because we are trying to access
+the var y outside its scope(The var are function scope the is
+there are only available till the execution of the function or block
+of code but can be accessed by the child due to scope
+chaining). So Due to function scoping of var we will get the
+reference error on Line 4
+*/
+
+//(e)
 let x=5;
 function first(){
     console.log(x);
@@ -58,3 +145,17 @@ function first(){
 
 first();
 console.log(y);
+
+/* This is the output if we will change the var with let
+Getting a output as follows
+console.log(x) as 5 //Line1
+console.log(y) as 10 //Line2
+console.log(z) as Reference Error //Line3
+console.log(y) will not get executed due to error //Line4
+
+We can clearly see that the let should be initialized before
+accessing it otherwise we will get Reference Error . It is due the
+temporal dead zone it is a state where the let and const are
+present without initialization in this case the hoisting of let z
+make it temporal dead zone.
+*/
